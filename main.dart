cc110c2950d8c5fa83406a7e3c6a1ee30ce9fa1f
Alexander Renaldi Christyanto <64631578+Arethusa01@users.noreply.github.com>
2023-02-17 12:12:19 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,143 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('A random AWESOME idea:'),
+          Text(appState.current.asLowerCase),
+          ElevatedButton(
+            onPressed: () {
+              print('button pressed!');
+            },
+            child: Text('Next'),
+          ),
+        ],      // ← 7
+      ),
+    );
+  }
+}
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+
+  // ↓ Add this.
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter(int counter) {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter = _counter + counter;
+    });
+  }
+
+  void main(){
+    runApp(MyApp());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Column(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+          children: [
+            Text('A random text'),
+            Text(
+              "$_counter",
+            ),
+
+            ElevatedButton(onPressed: (){
+              _incrementCounter(1);
+            }, child: Text('Add 1')),
+            ElevatedButton(onPressed: (){
+              _incrementCounter(2);
+            }, child: Text('Add 2')),
+            ElevatedButton(onPressed: (){
+              _incrementCounter(3);
+            }, child: Text('Add 3'))
+          ]
+
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){ _incrementCounter(9);},
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
